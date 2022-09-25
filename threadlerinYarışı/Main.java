fd343b20c6438c30d617f2f891679781c2663be0
Melike Hafsa Nazlı <melike.hafsa.nazli@gmail.com>
2022-09-25 22:05:00 +0300

    Add files via upload

---
+++ threadlerinYarışı/Main.java
@@ -0,0 +1,26 @@
+package threadlerinYarýþý;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ExecutorService execute = Executors.newFixedThreadPool(4);
+
+        ArrayList<Integer> array = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            array.add(i);
+        }
+
+        ThreadlerinYarýþý thr = new ThreadlerinYarýþý(array);
+
+        execute.execute(thr);
+
+
+
+    }
+}
---
+++ threadlerinYarışı/ThreadlerinYarışı.java
@@ -0,0 +1,42 @@
+package threadlerinYarýþý;
+
+import java.util.ArrayList;
+
+public class ThreadlerinYarýþý extends Thread{
+
+    private ArrayList<Integer> odd = new ArrayList<>();
+    private ArrayList<Integer> even = new ArrayList<>();
+    private ArrayList<Integer> general = new ArrayList<>();
+
+    public ThreadlerinYarýþý(ArrayList<Integer> general) {
+        this.general = general;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < general.size(); i++) {
+
+            if (general.get(i) % 2 == 0) {
+                even.add(general.get(i));
+            } else {
+                odd.add(general.get(i));
+            }
+
+        }
+
+        printOdd();
+        printEven();
+
+    }
+
+    public synchronized void printOdd() {
+        System.out.println(this.odd);
+
+    }
+
+    public synchronized void printEven() {
+        System.out.println(this.even);
+    }
+
+}
